ff352734f48a93162768d339b488ef7dbdbf8744
李飞 <897321637@qq.com>
2021-01-22 19:35:42 +0800

    第一次提交

---
+++ app/src/main/java/com/example/gitdemo/git.kt
@@ -0,0 +1,27 @@
+package com.example.gitdemo
+
+//自我介绍
+//git config --global user.name "张三"
+//git config --global user.email "897321637@qq.com"
+
+//查找该用户名称
+//git config --list
+
+//生成一个本地代码仓库
+//git init
+
+//生成忽略文件
+//git touch.ignore
+
+//查看状态哪些文件被上传和没有上传
+//git status
+
+//添加到本地仓库
+//单次添加
+//git add 文件名.class
+//全部添加
+//git add .
+//git add --all
+
+//提交
+//git commit -m "标记"
